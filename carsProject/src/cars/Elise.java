ee74cf3944571cd21de4cc0ebd2c36605f150168
Oltean Andrei-Florin <oltean.andrei.florin@gmail.com>
2015-11-17 16:12:59 +0200

    Added class and methods comments for the Elise class;

--- carsProject/src/cars/Elise.java
+++ carsProject/src/cars/Elise.java
@@ -1,4 +1,16 @@
 package cars;
+/**
+ * Elise class;
+ * Extends from Lotus class;
+ * 
+ * The class implements rules used in the calculation 
+ * of the fuel consumption and pollution created;
+ *
+ *It also has some predefined states like fuel tank size,fuel type etc.   
+ * 
+ * @author Oltean Andrei-Florin;
+ *
+ */
 
 public class Elise extends Lotus {
 	protected static final float FuelTankSize =60; //Factory specifications
@@ -7,7 +19,7 @@ public class Elise extends Lotus {
 	protected static final float Pollution=149; //Factory specifications
 	protected static final float FuelConsumedAtStart=0.23f;//Custom consumption of fuel at the start of the motor may vary from reality
 	protected static final float FuelConsumptionAvarage = 6.3f;//Factory specifications
-	protected final String ChassisNumber;
+	protected final String ChassisNumber;//
 
 	public String getChassisNumber() {
 		return this.ChassisNumber;
@@ -15,7 +27,9 @@ public class Elise extends Lotus {
 	
 	
 
-
+/**
+ * Returns the fuel consumption /100km in a given gear;
+ */
 	@Override
 	protected final float calculateFuelConsumptionAlgorithm(int gear) {
 		
@@ -38,22 +52,35 @@ public class Elise extends Lotus {
 		}
 	}
 
-
+/**
+ * calculates the pollution created for a given 
+ * quantity of fuel consumed;
+ */
 	@Override
 	protected float getCo2Emissions(float currentFuelConsumption) {
 		float currentCo2Emmisions = currentFuelConsumption * Pollution / FuelConsumptionAvarage;
 		return currentCo2Emmisions;
 	}
+	/**
+	 * Gets the fuel consumed at start (Its a constant);
+	 */
 	@Override
 	protected float getFuelConsumedAtStart() {
 		return FuelConsumedAtStart;
 	}
+	/**
+	 * calculates the pollution created during start;
+	 */
 	@Override
 	protected float calculateStarPollution() {
 		float co2Produced =FuelConsumedAtStart * 2392;  //1 L of gasoline produces 2392 g of co2
 		return co2Produced;
 	}
-	
+	/**
+	 * constructor for the Elise class;
+	 * @param availableFuel
+	 * @param chassisNumber
+	 */
 	public Elise(double availableFuel,String chassisNumber){
 		super(FuelTankSize, FuelType, MaxNumberOfGears, (float)availableFuel);
 		if (availableFuel>FuelTankSize ){
