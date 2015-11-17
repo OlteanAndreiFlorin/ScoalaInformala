a2ebba205fc15300c342d61f2a260abe2a817468
Oltean Andrei-Florin <oltean.andrei.florin@gmail.com>
2015-11-17 15:42:36 +0200

    Added method comments;

--- carsProject/src/cars/Lotus.java
+++ carsProject/src/cars/Lotus.java
@@ -6,11 +6,20 @@ package cars;
  * implements 2 abstract methods: calculateFuelConsumptionAlgorithm() and getCo2Emissions(); 
  */
 public abstract class Lotus extends Car {
-
+/**
+ * Constructor for the Lotus clas;
+ * @param fuelTankSize
+ * @param fuelType
+ * @param maxNumberOfGears
+ * @param availableFuel
+ */
 	public Lotus(float fuelTankSize, String fuelType, int maxNumberOfGears, float availableFuel) {
 		super(fuelTankSize, fuelType, maxNumberOfGears, availableFuel);
 	}
 
+	/**
+	 * Calculates the fuel consumed/100km this trip;
+	 */
 	@Override
 	protected float calculateFuelConsumption(float tripLength, int gear) {
 		float fuelConsumed = calculateFuelConsumptionAlgorithm(gear)/100 * tripLength;
@@ -18,7 +27,9 @@ public abstract class Lotus extends Car {
 	}
 	
 	protected abstract float calculateFuelConsumptionAlgorithm(int gear);
-	
+	/**
+	 * Calculates the average pollution/100km created this trip;
+	 */
 	protected float calculatePollution(float tripLength,float fuelConsumed){
 		float avarageFuelConsumptionPer100KmThisTrip = fuelConsumed / tripLength *100;
 		return getCo2Emissions(avarageFuelConsumptionPer100KmThisTrip);
