3d385ad80222351da7a01ff9eaabc3365a033490
Oltean Andrei-Florin <oltean.andrei.florin@gmail.com>
2015-11-19 00:03:04 +0200

    Changed CarWashMain (Extracet in to a method)

--- carsProject/src/user/CarWashMain.java
+++ carsProject/src/user/CarWashMain.java
@@ -1,11 +1,67 @@
 package user;
 import car_wash.CarInventory;
+import car_wash.CarWash;
 import cars.*;
 import java.util.List;
 
 public class CarWashMain {
 
 	public static void main(String[] args) { 
+		runCarInventoryTest();
+		
+		runCarWashTest();
+	}
+
+	public static void runCarWashTest() {
+		Car car1 = new ModelS(50,"CarS");
+		Car car2 = new ModelX(50,"CarS");
+		Car car3 = new Elise(50,"CarEl");
+		
+		CarWash carWash = new CarWash();
+
+		carWash.openShop();
+
+		carWash.standInLine(car1); // car stands in line to get washed.
+
+		carWash.standInLine(car2); // car2 stands in line to get washed.
+
+		carWash.washCar(); // will wash the first car standing in line, which is car1
+
+		carWash.standInLine(car3);
+
+		carWash.washCar(); // will wash the next car standing in line, which is car2
+
+		carWash.standInLine(car1);
+
+		carWash.washCar();
+		
+		carWash.washCar();
+		carWash.washCar();
+		carWash.washCar();
+		carWash.washCar();
+		carWash.washCar();
+
+		carWash.standInLine(car2);
+
+		carWash.standInLine(car3);
+
+		carWash.standInLine(car3);
+
+		carWash.closeShop();
+
+		List<Car> todaysClients = carWash.getTodaysClients(); // returns all clients that have visited the shop today. If car was washed twice, it should be returned once.
+
+		for (Car i : todaysClients){
+			System.out.println("the cars are: "+i);
+		}
+		System.out.println();
+		List<Car> postponedClients = carWash.getPostponedClients(); // returns a list of clients who did not get to have their car washed. The shop closed while they were standing in line.
+		for (Car i : postponedClients){
+			System.out.println("the cars are: "+i);
+		}
+	}
+
+	public static void runCarInventoryTest() {
 		Car car1 = new ModelS(50,"CarS");
 		Car car2 = new ModelX(50,"CarS");
 		Car car3 = new Elise(50,"CarEl");
